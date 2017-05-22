92807faf5004aa757bf65d84ef86dd39bf15e130
undancer <undancer@gmail.com>
2017-05-23 01:59:12 +0800

    拆分成独立对象

---
+++ ui/src/main/java/com/example/config/FilterConfig.java
@@ -0,0 +1,21 @@
+package com.example.config;
+
+import org.apache.catalina.filters.RequestDumperFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by undancer on 2017/5/23.
+ */
+@Configuration
+public class FilterConfig {
+
+
+    @Profile("!cloud")
+    @Bean
+    RequestDumperFilter requestDumperFilter() {
+        return new RequestDumperFilter();
+    }
+
+}
---
+++ ui/src/main/java/com/example/web/HomeController.java
@@ -0,0 +1,45 @@
+package com.example.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.RequestEntity;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by undancer on 2017/5/23.
+ */
+@Controller
+public class HomeController {
+    
+    @Autowired
+    OAuth2RestTemplate restTemplate;
+    @Value("${messages.url:http://localhost:7777}/api")
+    String messagesUrl;
+
+    @RequestMapping("/")
+    String home(Model model) {
+        List<Message> messages = Arrays.asList(restTemplate.getForObject(messagesUrl + "/messages", Message[].class));
+        model.addAttribute("messages", messages);
+        return "index";
+    }
+
+    @RequestMapping(path = "messages", method = RequestMethod.POST)
+    String postMessages(@RequestParam String text) {
+        Message message = new Message();
+        message.text = text;
+        restTemplate.exchange(RequestEntity
+                .post(UriComponentsBuilder.fromHttpUrl(messagesUrl).pathSegment("messages").build().toUri())
+                .body(message), Message.class);
+        return "redirect:/";
+    }
+
+}
