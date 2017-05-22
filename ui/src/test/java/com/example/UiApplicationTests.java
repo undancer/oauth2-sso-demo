4e52e16b03cd46ab4d6dd375307e612831f57ce2
undancer <undancer@gmail.com>
2017-05-23 02:01:26 +0800

    纠错

--- ui/src/test/java/com/example/UiApplicationTests.java
+++ ui/src/test/java/com/example/UiApplicationTests.java
@@ -7,12 +7,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = UiApplication.class)
+@SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 public class UiApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
